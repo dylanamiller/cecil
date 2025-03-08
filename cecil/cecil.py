121fd81d10ef91a541152fdd7b5e1ba4c2efa628
dylan miller <dylanamiller3@gmail.com>
2025-03-08 18:27:01 -0500

    update rag

--- cecil/cecil.py
+++ cecil/cecil.py
@@ -9,10 +9,14 @@ from PyPDF2 import PdfReader
 from IPython.core.magic import Magics, magics_class, line_magic, cell_magic
 from IPython import get_ipython
 
-from embedchain import BotAgent
-from embedchain.vector_stores import Weaviate
+from langchain_community.embeddings.ollama import OllamaEmbeddings
 from langchain_ollama import OllamaLLM
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.document_loaders.pdf import PyPDFDirectoryLoader
 from langgraph.graph import StateGraph, START, END
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.document import Document
+from langchain.vectorstores.chroma import Chroma
 
 from cecil.state.agent_state import AgentState
 from cecil.utils.system_prompt import SYSTEM_PROMPT
@@ -29,20 +33,17 @@ class Cecil(Magics):
 
     @line_magic
     def initialize(self, line: dict[str, Any]) -> None:
-        vector_store = Weaviate()
-        temp_dir = tempfile.mkdtemp()
         if line.get("model", None):
             models = OmegaConf.create({
                 model_fcn: OllamaLLM(model=model_name) \
                     for model_name, model_fcn in line.models.items()
             })
-            rag_llm = OllamaLLM(model=line.rag_model)
-            models.rag = BotAgent(llm=rag_llm, vector_store=vector_store, temp_dir=temp_dir)
+            models.rag = OllamaLLM(model=line.rag_model)
         else:
             models = OmegaConf.create({})
             llm = OllamaLLM(model="deepseek-r1:70b")
             models.model = llm
-            models.rag = BotAgent(llm=llm, vector_store=vector_store, temp_dir=temp_dir)
+            models.rag = llm
         self._models = models
         state = AgentState(
             input="",
@@ -66,6 +67,10 @@ class Cecil(Magics):
             "up",
             "-d",
         ], check=False)
+        # rag
+        self._chunk_size = line.get("chunk_size", None)
+        self._chunk_overlap = line.get("chunk_overlap", None)
+        self._chroma_path = line.get("chroma_path", None)
 
     def _chat(self, state: AgentState) -> str:
         messages = state['messages'] + state["system_prompt"]
@@ -78,6 +83,55 @@ class Cecil(Magics):
             model = "model"
         return self._models[model].invoke(query)
 
+    def _query_rag(self, query: str) -> str:
+        db = Chroma(
+            persist_directory=self._chroma_path, embedding_function=self._get_embedding_fcn()
+        )
+        results = db.similarity_search_with_score(query, k=5)
+        context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+        prompt_template = ChatPromptTemplate.from_template(SYSTEM_PROMPT)
+        prompt = prompt_template.format(context=context_text, question=query)
+        return self._models.rag.invoke(prompt)
+
+    def _add_to_chroma(self, chunks: list[Document]) -> None:
+        db = Chroma(
+            persist_directory=self._chroma_path, embedding_function=self._get_embedding_fcn()
+        )
+        existing_items = db.get(include=[])
+        existing_ids = set(existing_items["ids"])
+        new_chunks = []
+        new_chunk_ids = []
+        last_page_id = None
+        current_page_idx = 0
+        for chunk in chunks:
+            source = chunk.metadata.get("source")
+            page = chunk.metadata.get("page")
+            current_page_id = f"{source}:{page}#"
+            if last_page_id == current_page_id:
+                current_page_idx += 1
+            else:
+                current_page_idx = 0
+            last_page_id = current_page_id
+            uid = current_page_id + str(current_page_idx)
+            chunk.metadata["id"] = uid
+            if uid not in existing_ids:
+                new_chunk_ids.append(uid)
+                new_chunks.append(chunk)
+        db.add_documents(new_chunks, ids=new_chunk_ids)
+        db.persist()
+
+    def _split_docs(self, documents: list[Document]) -> list[Document]:
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=self._chunk_size,
+            chunk_overlap=self._chunk_overlap,
+            length_function=len,
+            is_separator_regex=False,
+        )
+        return text_splitter.split_documents(documents)
+
+    def _get_embedding_fcn(self) -> OllamaEmbeddings:
+        return OllamaEmbeddings(model="nomic-embed-test")
+
     @line_magic
     def chat(self, line: dict[str, str]) -> str:
         if not line.get("model_called", None):
@@ -89,16 +143,9 @@ class Cecil(Magics):
         return self._query(line[0], line[1])
 
     @line_magic
-    def store_pdf(self, line: os.PathLike) -> None:
-        reader = PdfReader(line)
-        text = ""
-        for page in reader.pages:
-            text += page.extract_text()
-        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-            temp_file.write(text)
-            temp_file_path = temp_file.name
-        # Add the PDF file to the knowledge base
-        self._model.rag.add_source(temp_file_path)
+    def load_pdf(self, line: os.PathLike) -> None:
+        document_loader = PyPDFDirectoryLoader(line)
+        document = document_loader.load()
 
     @line_magic
     def pdfquery(self, line: str) -> str:
--- cecil/utils/system_prompt.py
+++ cecil/utils/system_prompt.py
@@ -1,1 +1,9 @@
-SYSTEM_PROMPT = ""
+SYSTEM_PROMPT = """
+Answer the question using the following context as much as possible:
+
+{context}
+
+---
+
+Answer the following question using the above context: {question}
+"""
